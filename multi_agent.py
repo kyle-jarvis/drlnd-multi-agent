f1da7afc4786306ef2788790ae1512f35bfc7058
Kyle Jarvis <kyle.jarvis1905@gmail.com>
2021-05-04 11:32:57 +0100

    feat: changes to reflect refactoring of commons

--- multi_agent.py
+++ multi_agent.py
@@ -3,33 +3,25 @@ Multi agent projects from Udacity's DRLND
 """
 
 from unityagents import UnityEnvironment
-from drlnd.common.agents.utils import ReplayBuffer, ActionType
-from drlnd.common.agents.maddpg import MADDPGAgent, AgentSpec
-from drlnd.common.utils import get_next_results_directory, path_from_project_home, OrnsteinUhlenbeckProcess
-from collections import deque
+from drlnd.common.agents.utils import (
+    ReplayBuffer,
+    ActionType,
+    get_unity_env,
+    AgentInventory,
+    UnityEnvWrapper,
+)
+from drlnd.common.agents.maddpg import MADDPGAgent2
+from drlnd.common.utils import (
+    get_next_results_directory,
+    path_from_project_home,
+    OrnsteinUhlenbeckProcess,
+)
+from collections import deque, namedtuple
 import click
 import torch
 import numpy as np
 import os
 import yaml
-
-
-def get_unity_env(path: str):
-    env = UnityEnvironment(file_name=path)
-
-    brain_names = env.brain_names
-    brains = [env.brains[x] for x in brain_names]
-    env_infos = [env.reset(train_mode=False)[x] for x in brain_names]   
-    num_agents = [len(x.agents) for x in env_infos]
-
-    action_sizes = [x.vector_action_space_size for x in brains]
-    states = [x.vector_observations for x in env_infos]
-    state_sizes = [x.shape[1] for x in states]
-
-    #[print(f"Num agents: {num_agents}\nAction dim: f{action_size}\nState dim: {state_size}")]
-
-    return env, brain_names, num_agents, action_sizes, state_sizes
-
 
 
 @click.group()
@@ -38,99 +30,135 @@ def cli():
 
 
 @cli.command()
-@click.option("--n-episodes", type=int, default = 1, 
+@click.option(
+    "--n-episodes",
+    type=int,
+    default=1,
     help="""
     Number of episodes to train for.
-    """)
-@click.option("--note", type=str, default=None, 
+    """,
+)
+@click.option(
+    "--note",
+    type=str,
+    default=None,
     help="""
     Note to record to .txt file when results are saved.
-    """)
-def train(n_episodes, note):
-    """Train a pair of agents to play tennis using the MADDPG algorithm.
-    """
-    env, brain_names, num_agents, action_sizes, state_sizes = \
-        get_unity_env(
-            os.path.join(
-                os.environ['PROJECT_HOME'], 
-                './unity_environments/Soccer/Soccer_Linux/Soccer.x86_64'
-                )
-            )
+    """,
+)
+@click.option(
+    "--weights-path",
+    type=str,
+    default=None,
+    help="""
+    Path to the directory containing the trained weights of the agents networks.
+    Can be none, in which case the pre-trained weights in resources are used.
+    """,
+)
+def train(n_episodes, note, weights_path):
+    """Train a pair of agents to play tennis using the MADDPG algorithm."""
+    env, brain_spec = get_unity_env(
+        os.path.join(
+            os.environ["PROJECT_HOME"],
+            "./unity_environments/Tennis/Tennis_Linux/Tennis.x86_64",
+        )
+    )
 
+    brain_spec_list = list(brain_spec.values())
     print(env.brain_names)
 
-    buffer = ReplayBuffer(
-        action_sizes, 
-        int(5e6), 
-        256, 
-        1234, 
-        action_dtype = ActionType.CONTINUOUS
-        )
+    env_wrapper = UnityEnvWrapper(env, brain_spec_list, ActionType.CONTINUOUS)
 
-    agent_specs = []
-    for i, brain_name in enumerate(brain_names):
-        print(f"Collecting agent specs for {brain_name}")
-        for j in range(num_agents[i]):
-            agent_specs.append(AgentSpec(state_sizes[i], action_sizes[i]))
-    agent = MADDPGAgent(agent_specs, buffer, hidden_layer_size=256)
-    print(agent_specs)
+    # This defines an important ordering which sets which indices of the policy
+    # and critic networks correspond to the observations and actions of each agent.
+    # This list is passed to the agent inventory, which assigns slices to agent specs
+    # and also is passed to the buffer.observe_environment method.
+
+    buffer = ReplayBuffer(
+        int(5e6),
+        256,
+        1234,
+        action_dtype=ActionType.CONTINUOUS,
+        brain_agents=brain_spec_list,
+    )
+
+    agent_inventory = AgentInventory(brain_spec_list)
+    agent = MADDPGAgent2(agent_inventory, buffer, hidden_layer_size=256)
+
+    if weights_path is not None:
+        agent.load_weights(weights_path)
 
     episode_scores = deque(maxlen=100)
     on_policy_scores = deque(maxlen=100)
     average_scores = []
     on_policy_averages = []
-    online = lambda x: (x%10 == 0)
+    online = lambda x: (x % 10 == 0)
     learning_episodes = 0
 
     noise_fn_taper = 10000
     scale_next = 1.0
+    exception_raised = None
 
     for i in range(n_episodes):
         try:
             print(f"Episode: {i}")
-            env_info = env.reset(train_mode=True)[brain_name]
-            state = env_info.vector_observations
-            scores = [0.0 for x in agent_specs]
+            env_wrapper.reset(train_mode=True)
+            scores = [0.0 for x in range(agent_inventory.num_agents)]
 
-            if not online(i):  
+            if not online(i):
                 # Include some noise in the action selection, which we linearly scale
-                scale = max([1e-4, (1.0-(float(learning_episodes)/noise_fn_taper))])
-                #noise_fn = lambda : torch.from_numpy(np.random.normal(loc=0.0, scale=scale, size=(action_size))).float()
+                scale = max([1e-4, (1.0 - (float(learning_episodes) / noise_fn_taper))])
+                # noise_fn = lambda : torch.from_numpy(np.random.normal(loc=0.0, scale=scale, size=(action_size))).float()
                 dt = np.random.choice([1e-2, 5e-2, 1e-1])
                 theta = np.random.choice([0.1, 0.5, 1.0])
-                noise_generator = OrnsteinUhlenbeckProcess([action_size], scale, dt=dt, theta=theta)
-                noise_fn = lambda : torch.from_numpy(noise_generator.sample()).float()
+                noise_generator = OrnsteinUhlenbeckProcess(
+                    [brain_spec_list[0].action_size], scale, dt=dt, theta=theta
+                )
+                noise_fn = lambda: torch.from_numpy(noise_generator.sample()).float()
                 learning_episodes += 1
             else:
                 scale = 0.0
-                noise_fn = lambda : 0.0
-                
-            while True:
-                actions = (
-                    agent.act(
-                        torch.from_numpy(state).float(),
-                        policy_suppression = (1.0 - scale), 
-                        noise_func = noise_fn)
-                        )
+                noise_fn = lambda: 0.0
 
-                print(actions)
-                env = env.step(dict(zip(brain_names, actions)))
-                env_infos = [env[x] for x in brain_names]
-                next_states = [x.vector_observations for x in env_infos]
-                rewards = [x.rewards for x in env_infos]
-                dones = [x.local_done for x in env_infos]
+            steps_this_episode = 0
+
+            while True:
+                states = env_wrapper.get_states()
+                actions = agent.act(
+                    states, policy_suppression=(1.0 - scale), noise_func=noise_fn
+                )
+
+                # print(actions)
+                # print(states)
+                # Actions is now returned as {'brain1': [actions], 'brain2': [actions]}
+                # The order in which states and actions are concatenated here, then together
+                # determines the interpretation of the input neurons on the global critics
+                # It has to match the states and actions that have already happened
+                env_wrapper.step(vector_action=actions)
 
                 if not online(i):
-                    agent.replay_buffer.add(
-                        np.concatenate(state), 
-                        np.concatenate(actions), 
-                        rewards, 
-                        np.concatenate(next_states), 
-                        dones)
+                    (
+                        states,
+                        actions,
+                        rewards,
+                        next_states,
+                        dones,
+                    ) = agent.replay_buffer.add_from_dicts(*env_wrapper.sars())
                     agent.learn(0.99)
 
-                scores = [scores[x] + rewards[x] for x in range(num_agents)]
-                state = next_states
+                rewards = env_wrapper.get_rewards()
+                dones = env_wrapper.get_dones()
+                scores = [
+                    scores[x] + rewards["TennisBrain"][x]
+                    for x in range(agent_inventory.num_agents)
+                ]
+                dones = [
+                    dones["TennisBrain"][x] for x in range(agent_inventory.num_agents)
+                ]
+
+                steps_this_episode += 1
+                print(f"Steps taken this episode: {steps_this_episode}")
+                print(f"Dones = {dones}")
                 if np.any(dones):
                     break
 
@@ -141,80 +169,109 @@ def train(n_episodes, note):
                 on_policy_scores.append(scores)
                 on_policy_averages.append(np.mean(on_policy_scores))
 
-            print('Total score this episode: {}'.format(env_info.rewards))
-        except KeyboardInterrupt:
-            print("Interrupted training, will save weights now")
+            print("Total score this episode: {}".format(rewards))
+        except (Exception, KeyboardInterrupt) as event:
+            if isinstance(event, KeyboardInterrupt):
+                print("Interrupted training, will save weights now")
+            else:
+                print("Encountered an unexpected exception, trying to save weights")
+                exception_raised = event
             break
 
     results_directory = get_next_results_directory()
     agent.save_weights(results_directory)
-    np.savetxt(os.path.join(results_directory, 'scores.txt'), average_scores)
-    np.savetxt(os.path.join(results_directory, 'on_policy_scores.txt'), on_policy_averages)
+    np.savetxt(os.path.join(results_directory, "scores.txt"), average_scores)
+    np.savetxt(
+        os.path.join(results_directory, "on_policy_scores.txt"), on_policy_averages
+    )
     if note is not None:
-        with open(os.path.join(results_directory, 'note.txt'), 'w') as f:
+        with open(os.path.join(results_directory, "note.txt"), "w") as f:
             f.write(note)
     params = {
-        'noise_fn': repr(noise_fn),
-        'noise_fn_taper': noise_fn_taper, 
-        'taper': 'linear'}
-    with open(os.path.join(results_directory, 'params.yml'), 'w') as f:
+        "noise_fn": repr(noise_fn),
+        "noise_fn_taper": noise_fn_taper,
+        "taper": "linear",
+    }
+    with open(os.path.join(results_directory, "params.yml"), "w") as f:
         yaml.dump(params, f)
+    print(f"INFO: Saved results to {results_directory}")
+    if exception_raised is not None:
+        raise event
 
 
 @cli.command()
-@click.option("--weights-path", type=str, default=None, 
+@click.option(
+    "--weights-path",
+    type=str,
+    default=None,
     help="""
     Path to the directory containing the trained weights of the agents networks.
     Can be none, in which case the pre-trained weights in resources are used.
-    """)
-@click.option('--n-episodes', type=int, default=1, 
-    help = """
+    """,
+)
+@click.option(
+    "--n-episodes",
+    type=int,
+    default=1,
+    help="""
     Number of episodes to train an agent for.
-    """)
+    """,
+)
 def run(weights_path: str, n_episodes: int):
-    """Initialise an agent using pre-trained network weights and observe the 
+    """Initialise an agent using pre-trained network weights and observe the
     agent's interaction with the environment.
     """
-    env, brain_name, num_agents, action_size, state_size = \
-        get_unity_env(
-            os.path.join(
-                os.environ['PROJECT_HOME'], 
-                './unity_environments/Tennis/Tennis_Linux/Tennis.x86_64'
-                )
-            )
-            
-    agent_specs = []
-    for i in range(num_agents):
-        agent_specs.append(AgentSpec(state_size, action_size))
-    agent = MADDPGAgent(agent_specs, None, hidden_layer_size=128)
+    env, brain_spec = get_unity_env(
+        os.path.join(
+            os.environ["PROJECT_HOME"],
+            "./unity_environments/Tennis/Tennis_Linux/Tennis.x86_64",
+        )
+    )
+
+    brain_spec_list = list(brain_spec.values())
+    print(env.brain_names)
+
+    env_wrapper = UnityEnvWrapper(env, brain_spec_list, ActionType.CONTINUOUS)
+
+    # This defines an important ordering which sets which indices of the policy
+    # and critic networks correspond to the observations and actions of each agent.
+    # This list is passed to the agent inventory, which assigns slices to agent specs
+    # and also is passed to the buffer.observe_environment method.
+
+    agent_inventory = AgentInventory(brain_spec_list)
+    agent = MADDPGAgent2(agent_inventory, hidden_layer_size=256)
 
     if weights_path is None:
-        weights_path = path_from_project_home('./resources/solved_weights/')
+        weights_path = path_from_project_home("./resources/solved_weights/")
     agent.load_weights(weights_path)
 
     for i in range(n_episodes):
         print(f"Episode: {i}")
-        env_info = env.reset(train_mode=False)[brain_name]
-        states = env_info.vector_observations
+        env_wrapper.reset(train_mode=False)
 
-        score = 0.0
+        scores = [0.0 for x in range(agent_inventory.num_agents)]
         for i in range(500):
-            actions = (
-                agent.act(
-                    torch.from_numpy(states).float(), 
-                    policy_suppression=1.0)
-                    )
-            env_info = env.step([x.numpy() for x in actions])[brain_name]
-            states = env_info.vector_observations
-            reward = env_info.rewards
-            done = env_info.local_done
+            states = env_wrapper.get_states()
+            actions = agent.act(states, policy_suppression=1.0)
 
-            score += env_info.rewards[0]
-            if np.any(done):
+            env_wrapper.step(vector_action=actions)
+
+            rewards = env_wrapper.get_rewards()
+            dones = env_wrapper.get_dones()
+            scores = [
+                scores[x] + rewards["TennisBrain"][x]
+                for x in range(agent_inventory.num_agents)
+            ]
+            dones = [dones["TennisBrain"][x] for x in range(agent_inventory.num_agents)]
+            if np.any(dones):
                 break
-        print('Total score (averaged over agents) this episode: {}'.format(np.mean(score)))
+        print(
+            "Total score (averaged over agents) this episode: {}".format(
+                np.mean(scores)
+            )
+        )
 
 
 if __name__ == "__main__":
-    os.environ['PROJECT_HOME'] = os.path.dirname(__file__)
+    os.environ["PROJECT_HOME"] = os.path.dirname(__file__)
     cli()
